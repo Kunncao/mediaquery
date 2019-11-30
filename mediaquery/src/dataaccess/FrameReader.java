eee6421080d8bf7401b611963b117ba1463a9580
Jianfei Cao <jianfeic@usc.edu>
2019-11-30 11:20:57 -0800

    Update FrameReader.java: throw exceptions

--- mediaquery/src/dataaccess/FrameReader.java
+++ mediaquery/src/dataaccess/FrameReader.java
@@ -13,51 +13,45 @@ public class FrameReader {
 	/**
 	 * read image from a .rgb file in a designated resolution(352 * 288)
 	 */
-	public BufferedImage readFrameImg(String path) {
+	public BufferedImage readFrameImg(String path) throws FileNotFoundException, IOException {
 		return readFrameImg(width, height, path);
 	}
 	
 	/**
 	 * read image from a .rgb file in a designated path with 8-bit RGB color 
 	 */
-	public static BufferedImage readFrameImg(int width, int height, String path) {
+	public static BufferedImage readFrameImg(int width, int height, String path) throws FileNotFoundException, IOException {
 		BufferedImage res = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		
-		try {
-			// read file
-			File imgf = new File(path);
-			// image resolution
-			int offset = width * height; 
-			// pixel value in r, g, b
-			int frameLen = offset * 3;
-			
-			RandomAccessFile raf = new RandomAccessFile(imgf, "r");
-			raf.seek(0);
-			
-			// get the raster
-			byte[] data = new byte[frameLen];
-			raf.read(data);
-			
-			// set rgb value for each pixel
-			int rafInd = 0;
-			// for blue
-			int offset2 = offset * 2;
-			for (int y = 0; y < height; y++) {
-				for (int x = 0; x < width; x++) {
-					byte r = data[rafInd], g = data[rafInd + offset], b = data[rafInd + offset2];
-					// & 0xff keep the MSB when casting from byte to int
-					int rgb = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-					res.setRGB(x, y, rgb);
-					rafInd++;
-				}
+		// read file
+		File imgf = new File(path);
+		// image resolution
+		int offset = width * height; 
+		// pixel value in r, g, b
+		int frameLen = offset * 3;
+		
+		RandomAccessFile raf = new RandomAccessFile(imgf, "r");
+		raf.seek(0);
+		
+		// get the raster
+		byte[] data = new byte[frameLen];
+		raf.read(data);
+		
+		// set rgb value for each pixel
+		int rafInd = 0;
+		// for blue
+		int offset2 = offset * 2;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				byte r = data[rafInd], g = data[rafInd + offset], b = data[rafInd + offset2];
+				// & 0xff keep the MSB when casting from byte to int
+				int rgb = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+				res.setRGB(x, y, rgb);
+				rafInd++;
 			}
-			
-			raf.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		
+		raf.close();
 		
 		return res;
 	}
