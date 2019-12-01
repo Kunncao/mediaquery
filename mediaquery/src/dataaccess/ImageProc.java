53a339fc58b8341591541f78aa0b2672ffdd6973
Jianfei Cao <jianfeic@usc.edu>
2019-12-01 12:57:30 -0800

    add resize() to ImageProc

--- mediaquery/src/dataaccess/ImageProc.java
+++ mediaquery/src/dataaccess/ImageProc.java
@@ -2,11 +2,67 @@ package dataaccess;
 
 public class ImageProc {
 	/**
+	 * a one dimension rgb raster to 2 dims matrix for r, g, b (0, 1, 2)
+	 */
+	public static byte[][][] raster2mat(int w, int h, byte[] src) {
+		int channels = 3;
+		// height * width * (0 r, 1 g, 2 b)
+		byte[][][] dst = new byte[h][w][channels];
+		int offset = w * h, offset2 = offset * 2;
+		
+		int i = 0;
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				// r
+				dst[y][x][0] = src[i];
+				// g
+				dst[y][x][1] = src[i + offset];
+				// b
+				dst[y][x][2] = src[i + offset2];
+				i++;	
+			}
+		}
+		
+		return dst;
+	}
+	
+	/**
 	 * resize an rgb image, return 3 color's matrix  
+	 * @param oriW original width
 	 * @param w width after resizing
 	 * @param ori original raster
 	 */
-	public static byte[] resize(int w, int h, byte[] ori) {
-		return ori;
+	public static byte[][][] resize(int srcW, int srcH, int dstW, int dstH, byte[][][] src) {
+		int channels = 3;
+		byte[][][] dst = new byte[dstH][dstW][channels];
+		// ratio of width and height
+		double rW = srcW * 1.0 / dstW,  rH = srcH * 1.0 / dstH;
+		
+		// resize
+		for (int y = 0; y < dstH; y++) {
+			for (int x = 0; x < dstW; x++) {
+				// corresponding x,y in the src
+				int srcX = (int)(x * rW), srcY = (int)(y * rH);
+				// r
+				dst[y][x][0] = src[srcY][srcX][0];
+				// g
+				dst[y][x][1] = src[srcY][srcX][1];	
+				// b
+				dst[y][x][2] = src[srcY][srcX][2];
+			}
+		}
+		
+		return dst;
+	}
+	
+	/**
+	 * convert rgb raster data to gray data
+	 */
+	public static byte[][] cvt2gray(int w, int h, byte[][][] src) {
+		byte[][] dst = new byte[h][w];
+		
+		
+		
+		return dst;
 	}
 }
