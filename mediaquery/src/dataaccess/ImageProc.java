d8d296812349f768c121cbd63ae294771b0a293f
Jianfei Cao <jianfeic@usc.edu>
2019-12-01 10:24:48 -0800

    Create ImageProc.java

---
+++ mediaquery/src/dataaccess/ImageProc.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+public class ImageProc {
+	/**
+	 * resize an rgb image, return 3 color's matrix  
+	 * @param w width after resizing
+	 * @param ori original raster
+	 */
+	public static byte[] resize(int w, int h, byte[] ori) {
+		return ori;
+	}
+}
