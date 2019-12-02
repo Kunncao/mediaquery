f1e92ad7e32b7360e82a2fef79f672effab5d0fd
Jianfei Cao <jianfeic@usc.edu>
2019-12-02 15:07:40 -0800

    remove getDistance in ColorDes

--- mediaquery/src/dataaccess/descriptor/ColorDes.java
+++ mediaquery/src/dataaccess/descriptor/ColorDes.java
@@ -1,19 +1,5 @@
 package dataaccess.descriptor;
 
 public class ColorDes {
-	/**
-	 * compute the Euclidean distance of two given points
-	 */
-	public static double getDistance(double[] p1, double[] p2) {
-		// dimension
-		int d = p1.length;
-		double sumOfSquare = 0; 
-		
-		for (int i = 0; i < d; i++) {
-			double dist = p1[i] - p2[i];
-			sumOfSquare += dist * dist;
-		}
-		
-		return Math.sqrt(sumOfSquare);
-	}
+	
 }
