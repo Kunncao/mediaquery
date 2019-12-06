80eaba5a051287d32a169a7fe47f3bdd99bf4b38
Jianfei Cao <jianfeic@usc.edu>
2019-12-06 07:04:53 -0800

    Update FreqComp.java

--- mediaquery/src/comparator/FreqComp.java
+++ mediaquery/src/comparator/FreqComp.java
@@ -1,12 +1,36 @@
 package comparator;
 
+import java.util.List;
+
+import dataaccess.descriptor.FreqDes;
 import model.Video;
 
 public class FreqComp {
-	public double compare(Video qV, Video dbV) {
+	public static double compare(Video qV, Video dbV) {
 		double sim = 0;
-		// TODO: compare fingerprint
+		// compare fingerprint
+		// query's fingerprint
+		List<String> qfp = qV.getFingerprint();
+		// database video fingerprint
+		List<String> dbfp = dbV.getFingerprint();
+		// compare in order (move the clip like a window)
+		int move = dbfp.size() - qfp.size();
+		for (int i = 0; i <= move; i++) {
+			// current sim
+			double curr = 0;
+			for (int j = 0; j < qfp.size(); j++) {
+				if (isSim(qfp.get(j), dbfp.get(i + j)))
+					curr += 1.0 / qfp.size();
+			}
+			sim = Math.max(curr, sim);
+		}
 		
 		return sim;
+	}
+	
+	public static boolean isSim(String fp1, String fp2) {
+		double threshold = 0.55;
+		if (FreqDes.similarity(fp1, fp2) > threshold) return true;
+		else return false;
 	}
 }
