4a730d6c819fc922c9978738f6e20649a1454ce3
Jianfei Cao <jianfeic@usc.edu>
2019-12-05 21:55:01 -0800

    Create FreqComp.java

---
+++ mediaquery/src/comparator/FreqComp.java
@@ -0,0 +1,12 @@
+package comparator;
+
+import model.Video;
+
+public class FreqComp {
+	public double compare(Video qV, Video dbV) {
+		double sim = 0;
+		// TODO: compare fingerprint
+		
+		return sim;
+	}
+}
