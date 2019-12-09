f42921a3daf2820a110d02f5c4ce94dea75cc07d
Jianfei Cao <jianfeic@usc.edu>
2019-12-09 11:24:58 -0800

    add total avg rms

--- mediaquery/src/dataaccess/descriptor/AudioDes.java
+++ mediaquery/src/dataaccess/descriptor/AudioDes.java
@@ -1,13 +1,37 @@
 package dataaccess.descriptor;
 
-import org.openimaj.audio.analysis.FourierTransform;
-import org.openimaj.video.xuggle.XuggleAudio;
+import org.openimaj.audio.AudioStream;
+import org.openimaj.audio.SampleChunk;
+import org.openimaj.audio.analysis.EffectiveSoundPressure;
+
+import util.VideoConst;
 
 public class AudioDes {
+	/** Number of frames average to take sound pressure over */
+	private final static int nFrameAvg = 1;
+	
 	/**
-	 * do the fast fourier transfer
+	 * @param as pass a XuggleAudio obj
+	 * @return effective sound pressure
+	 * @throws Exception 
 	 */
-	public static void doFFT(XuggleAudio xa) {
-		FourierTransform fft = new FourierTransform(xa);
+	public static double getEffectiveSoundPressure(AudioStream as) throws Exception {
+		// average RMS for each chunk
+		double soundPressure = 0;
+		// set the time length of the window and overlap
+		int windowSizeMillis = (int) (nFrameAvg * 1.0 / VideoConst.FPS * 1000), overlapMillis = 0;
+		double RMS = 0;
+		
+		EffectiveSoundPressure esp = new EffectiveSoundPressure(as, windowSizeMillis, overlapMillis);
+		@SuppressWarnings("unused")
+		SampleChunk sc = null;
+		int i = 0;
+		while ((sc = esp.nextSampleChunk()) != null) {
+			RMS += esp.getEffectiveSoundPressure() / 20d;
+			i++;
+		}
+
+		soundPressure = RMS / i;
+		return soundPressure;
 	}
 }
